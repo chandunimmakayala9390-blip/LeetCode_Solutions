8d88431bb7ebc91ba387ee2d789541d20a07a9b3
chandunimmakayala9390-blip <chandunimmakayala9390@gmail.com>
2026-04-03 13:35:47 +0530

    Time: 222 ms (20%), Space: 161.1 MB (40%) - LeetHub

--- 3661-maximum-walls-destroyed-by-robots/3661-maximum-walls-destroyed-by-robots.java
+++ 3661-maximum-walls-destroyed-by-robots/3661-maximum-walls-destroyed-by-robots.java
@@ -1,107 +1,105 @@
-import java.util.*;
-
 class Solution {
+
     public int maxWalls(int[] robots, int[] distance, int[] walls) {
         int n = robots.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+        int[] num = new int[n];
+        Map<Integer, Integer> robotsToDistance = new HashMap<>();
 
-        int[][] bots = new int[n][2];
         for (int i = 0; i < n; i++) {
-            bots[i][0] = robots[i];
-            bots[i][1] = distance[i];
+            robotsToDistance.put(robots[i], distance[i]);
         }
-        Arrays.sort(bots, (a, b) -> a[0] - b[0]);
 
-        int[] W = walls.clone();
-        Arrays.sort(W);
+        Arrays.sort(robots);
+        Arrays.sort(walls);
 
-        int[][] botsFull = new int[n + 2][2];
-        botsFull[0] = new int[]{Integer.MIN_VALUE / 2, 0};
-        for (int i = 0; i < n; i++) botsFull[i + 1] = bots[i];
-        botsFull[n + 1] = new int[]{Integer.MAX_VALUE / 2, 0};
+        for (int i = 0; i < n; i++) {
+            int pos1 = upperBound(walls, robots[i]);
 
-        int total = botsFull.length;
-
-        // Walls sitting exactly on a robot are destroyed for free
-        // Count them separately so they don't fall into gap logic
-        int freeWalls = 0;
-        {
-            Set<Integer> robotPositions = new HashSet<>();
-            for (int[] b : bots) robotPositions.add(b[0]);
-            Set<Integer> counted = new HashSet<>();
-            for (int w : W) {
-                if (robotPositions.contains(w) && counted.add(w)) {
-                    freeWalls++;
-                }
+            int leftPos = 0;
+            if (i >= 1) {
+                int leftBound = Math.max(
+                    robots[i] - robotsToDistance.get(robots[i]),
+                    robots[i - 1] + 1
+                );
+                leftPos = lowerBound(walls, leftBound);
+            } else {
+                leftPos = lowerBound(
+                    walls,
+                    robots[i] - robotsToDistance.get(robots[i])
+                );
             }
-        }
+            left[i] = pos1 - leftPos;
 
-        int[] LL = new int[total - 1];
-        int[] RR = new int[total - 1];
-        int[] OO = new int[total - 1];
-
-        for (int i = 0; i < total - 1; i++) {
-            int lpos = botsFull[i][0],  ldist = botsFull[i][1];
-            int rpos = botsFull[i + 1][0], rdist = botsFull[i + 1][1];
-
-            // Walls STRICTLY between lpos and rpos (walls ON robots handled above)
-            int a = upperBound(W, lpos);      // first index > lpos
-            int b = lowerBound(W, rpos);      // first index >= rpos  → excludes rpos
-
-            if (a >= b) continue;
-
-            int gapLen = b - a;
-            int[] gap = Arrays.copyOfRange(W, a, b);
-
-            long leftReach  = (long) lpos + ldist;
-            long rightReach = (long) rpos - rdist;
-
-            LL[i] = upperBound(gap, (int) Math.min(leftReach,  (long)(Integer.MAX_VALUE)));
-            RR[i] = gapLen - lowerBound(gap, (int) Math.max(rightReach, (long)(Integer.MIN_VALUE)));
-
-            if (leftReach >= rightReach) {
-                int ol  = lowerBound(gap, (int) Math.max(rightReach, (long)(Integer.MIN_VALUE)));
-                int or_ = upperBound(gap, (int) Math.min(leftReach,  (long)(Integer.MAX_VALUE)));
-                OO[i] = Math.max(0, or_ - ol);
+            int rightPos = 0;
+            if (i < n - 1) {
+                int rightBound = Math.min(
+                    robots[i] + robotsToDistance.get(robots[i]),
+                    robots[i + 1] - 1
+                );
+                rightPos = upperBound(walls, rightBound);
+            } else {
+                rightPos = upperBound(
+                    walls,
+                    robots[i] + robotsToDistance.get(robots[i])
+                );
             }
+            int pos2 = lowerBound(walls, robots[i]);
+            right[i] = rightPos - pos2;
+
+            if (i == 0) {
+                continue;
+            }
+            int pos3 = lowerBound(walls, robots[i - 1]);
+            num[i] = pos1 - pos3;
         }
 
-        long f = 0;
-        long g = Long.MIN_VALUE / 2;
-
-        for (int i = 1; i < total - 1; i++) {
-            int gapLeft  = i - 1;
-            int gapRight = i;
-
-            long newF = Math.max(f, g) + LL[gapRight];
-            long newG = Math.max(
-                f + RR[gapLeft] - OO[gapLeft],
-                g + RR[gapLeft]
+        int subLeft = left[0];
+        int subRight = right[0];
+        for (int i = 1; i < n; i++) {
+            int currentLeft = Math.max(
+                subLeft + left[i],
+                subRight -
+                right[i - 1] +
+                Math.min(left[i] + right[i - 1], num[i])
             );
-
-            f = newF;
-            g = newG;
+            int currentRight = Math.max(
+                subLeft + right[i],
+                subRight + right[i]
+            );
+            subLeft = currentLeft;
+            subRight = currentRight;
         }
 
-        return (int) Math.max(f, g) + freeWalls;
+        return Math.max(subLeft, subRight);
     }
 
-    private int upperBound(int[] W, int val) {
-        int lo = 0, hi = W.length;
-        while (lo < hi) {
-            int mid = (lo + hi) >>> 1;
-            if (W[mid] <= val) lo = mid + 1;
-            else hi = mid;
+    private int lowerBound(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
         }
-        return lo;
+        return left;
     }
 
-    private int lowerBound(int[] W, int val) {
-        int lo = 0, hi = W.length;
-        while (lo < hi) {
-            int mid = (lo + hi) >>> 1;
-            if (W[mid] < val) lo = mid + 1;
-            else hi = mid;
+    private int upperBound(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] <= target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
         }
-        return lo;
+        return left;
     }
 }
