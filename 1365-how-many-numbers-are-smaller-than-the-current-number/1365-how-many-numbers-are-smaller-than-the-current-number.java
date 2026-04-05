ae8b12b3a68ef1b8c40ae39584c7f9e7e8096a87
chandunimmakayala9390-blip <chandunimmakayala9390@gmail.com>
2026-04-05 23:34:41 +0530

    Time: 9 ms (32.91%), Space: 45.5 MB (62.02%) - LeetHub

---
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+class Solution {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        int n=nums.length;
+        int res[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(nums[i]>nums[j] && i!=j)
+                {
+                    res[i]++;
+                }
+            }
+        }
+        return res;
+    }
+}
